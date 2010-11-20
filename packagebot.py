730f04a6dd7ead0426c7a641bf9b244afcc2d12a
Brant Gurganus <brant@gurganus.name>
2010-11-20 00:19:06 -0500

    modified to use multiprocessing

--- packagebot.py
+++ packagebot.py
@@ -1,18 +1,24 @@
 # -*- coding: utf-8; tab-width: 4; indent-tabs-mode: nil -*-
 import os
+import multiprocessing
 from xml.etree import ElementTree
 from argparse import ArgumentParser
 
+def process_xml(metatype, category, name, path):
+    return (name, category, metatype, ElementTree.parse(path))
+
 class PackageBot(object):
-    def __init__(self, verbose, tree):
+    def __init__(self, verbose, tree, pool):
         object.__init__(self)
-        self.verbose = True
+        self.verbose = verbose
         self.tree = tree
+        self.pool = pool
 
     def run(self):
         name = None
         category = None
         metatype = 'unknown'
+        results = list()
         for root, dirs, files in os.walk(self.tree):
             if 'metadata.xml' in files:
                 if os.path.dirname(root) == self.tree:
@@ -27,7 +33,11 @@ class PackageBot(object):
                     print 'Type: %(type)s' % {'type': metatype}
                     print 'Category: %(category)s' % {'category': category}
                     print 'Name: %(name)s' % {'name': name}
-                tree = ElementTree.parse(path);
+                results.append(self.pool.apply_async(process_xml,
+                    args = (metatype, category, name, path)))
+        for result in results:
+            result = result.get(None)
+            print result[0]
                 
 
 def main():
@@ -44,6 +54,13 @@ def main():
         dest = 'verbose',
         default = False,
         help = 'print details on what Packagebot is doing')
+    parser.add_argument('-j', '--jobs',
+        action = 'store',
+        dest = 'jobs',
+        type = int,
+        default = multiprocessing.cpu_count(),
+        help = 'run jobs in parallel',
+        nargs = '?')
     parser.add_argument('tree',
         action = 'store',
         default = '/usr/portage',
@@ -53,10 +70,14 @@ def main():
     if options.verbose:
         print 'Starting in verbose mode'
     tree = os.path.normpath(options.tree)
+    pool = multiprocessing.Pool(processes = options.jobs)
     if options.verbose:
         print 'Using portage tree at %(tree)s' % {'tree': tree}
-    bot = PackageBot(options.verbose, tree)
+        print 'Using %(jobs)u jobs' % {'jobs': options.jobs}
+    bot = PackageBot(options.verbose, tree, pool)
     bot.run()
+    pool.close()
+    pool.join()
 
 if __name__ == '__main__':
     main()
